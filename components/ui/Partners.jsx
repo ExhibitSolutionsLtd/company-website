18bb473a0dd3b225d65441f1a5794257e51fa716
Morces <karanim594@gmail.com>
2024-11-28 17:29:02 +0300

    Partners section

--- components/ui/Partners.jsx
+++ components/ui/Partners.jsx
@@ -9,7 +9,7 @@ const Partners = () => {
     { id: 4, url: "/assets/partners/entrust.png", alt: "Entrust Logo" },
     { id: 5, url: "/assets/partners/jabra.png", alt: "Jabra Logo" },
     { id: 6, url: "/assets/partners/juniper.png", alt: "Juniper Logo" },
-    { id: 7, url: "/assets/partners/Kaspersky.png", alt: "Kasperskey Logo" },
+    { id: 7, url: "/assets/partners/Kaspersky.png", alt: "Kaspersky Logo" },
     { id: 8, url: "/assets/partners/Lenel.png", alt: "Lenel Logo" },
     { id: 9, url: "/assets/partners/ruckus.png", alt: "Ruckus Logo" },
     { id: 10, url: "/assets/partners/schneider.png", alt: "Schneider Logo" },
@@ -17,20 +17,26 @@ const Partners = () => {
     { id: 12, url: "/assets/partners/suprema.png", alt: "Suprema Logo" },
     { id: 13, url: "/assets/partners/ZK_Teco.png", alt: "ZKTeco Logo" },
   ];
+
   return (
-    <div className="w-4/5 mx-auto p-5 mt-6 ">
+    <div className="w-4/5 mx-auto p-5 mt-6">
       <h3 className="text-center text-2xl text-primary font-semibold">
         Our Amazing Partners
       </h3>
-      <div className="flex justify-between md:flex-wrap mt-6 gap-4">
+      <div className="w-full flex justify-center md:justify-between items-center mt-6 gap-6 flex-wrap">
         {partners?.map((partner, i) => (
-          <Image
+          <div
             key={partner?.id || i}
-            src={partner?.url}
-            width={100}
-            height={20}
-            alt={partner?.alt}
-          />
+            className="flex justify-center items-center w-full sm:w-1/2 md:w-1/4 lg:w-1/6 xl:w-1/8 p-4"
+          >
+            <Image
+              src={partner?.url}
+              width={150}
+              height={100}
+              alt={partner?.alt}
+              className="object-contain"
+            />
+          </div>
         ))}
       </div>
     </div>
